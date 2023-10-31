cc76f5768f0d1ef142fefac2de18da57f42f326c
Harshal <86665758+harshal0001@users.noreply.github.com>
2023-10-31 14:08:02 +0530

    Bootcamps router created

--- server.js
+++ server.js
@@ -1,30 +1,16 @@
 const express = require('express'); 
 const dotenv = require('dotenv');
 
+// Route Files
+const bootcamps = require('./routes/bootcamps');
+
 // Load env vars
 dotenv.config({ path:'./config/config.env' });
 
 const app = express();
 
-app.get('/api/v1/bootcamps', (req, res) => {
-    res.status(200).json({ success: 'true', msg: 'Show All Bootcamps' });
-});
-
-app.get('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json({ success: 'true', msg: `Show Bootcamp ${req.params.id}` });
-});
-
-app.post('/api/v1/bootcamps', (req, res) => {
-    res.status(200).json({ success: 'true', msg: 'Create new Bootcamp' });
-});
-
-app.put('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json({ success: 'true', msg: `Update Bootcamp ${req.params.id}` });
-});
-
-app.delete('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json({ success: 'true', msg: `Delete Bootcamp ${req.params.id}` });
-});
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server Running in ${process.env.NODE_ENV} mode on port ${PORT}`));
