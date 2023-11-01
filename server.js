8264b2223c629ae1b5cc1e2966de301ce3dda96a
Harshal <86665758+harshal0001@users.noreply.github.com>
2023-11-01 13:13:01 +0530

    Mongo DB initialized

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
-const express = require('express'); 
+const express = require('express');
 const dotenv = require('dotenv');
+const logger = require('./middleware/logger');
+const connectDB = require('./config/db');
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+//Connect to DB
+connectDB();
 
 // Route Files
 const bootcamps = require('./routes/bootcamps');
 
-// Load env vars
-dotenv.config({ path:'./config/config.env' });
-
 const app = express();
+
+app.use(logger);
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server Running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server Running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+//Handle unhandled promise rejections
+process.on('unhandled rejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    //Close Server and Exit process
+    server.close(() => process.exit(1));
+});
