e927c34d70aab75d765464bf051d9bec13a97591
Harshal <86665758+harshal0001@users.noreply.github.com>
2023-11-02 12:01:49 +0530

    CREATE Bootcamps done

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const colors = require('colors');
 const logger = require('./middleware/logger');
 const connectDB = require('./config/db');
 
@@ -11,20 +12,24 @@ connectDB();
 
 // Route Files
 const bootcamps = require('./routes/bootcamps');
+const { connect } = require('mongoose');
 
 const app = express();
 
 app.use(logger);
 
+//Body Parser
+app.use(express.json());
+
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT, console.log(`Server Running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server Running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
 
 //Handle unhandled promise rejections
 process.on('unhandled rejection', (err, promise) => {
-    console.log(`Error: ${err.message}`);
+    console.log(`Error: ${err.message}`.red);
     //Close Server and Exit process
     server.close(() => process.exit(1));
 });
